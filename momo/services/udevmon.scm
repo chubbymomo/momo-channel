c158f7cac503a69c46bcebcf5b3eed51143b612c
Samuel Willey <samuel@thewilley.family>
2024-01-17 08:57:57 -0600

    attempting to fix udevmon service

--- momo/services/udevmon.scm
+++ momo/services/udevmon.scm
@@ -1,11 +1,13 @@
 (define-module (momo services linux))
 
 (define udevmon-service-type
-  (make <service>
-    #:provides '(udevmon)
-    #:docstring "Run udevmon"
-    #:start (make-forkexec-constructor
-             '("udevmon" "-c /etc/udevmon.yaml"))
-    #:stop (make-kill-destructor)
-    #:respawn? #t))
+  (service
+   '(udevmon)
+   #:provides '(udevmon)
+   #:docstring "Run udevmon"
+   #:start (make-forkexec-constructor
+            '("udevmon" "-c /etc/udevmon.yaml"))
+   #:stop (make-kill-destructor)
+   #:respawn? #t))
+
 (register-services udevmon)
