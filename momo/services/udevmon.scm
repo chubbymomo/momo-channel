84d9699f281de87ba356b4cfea9b57d9c1876684
Samuel Willey <samuel@thewilley.family>
2024-01-18 21:39:40 -0600

    Fixed constructor for udevmon

--- momo/services/udevmon.scm
+++ momo/services/udevmon.scm
@@ -1,36 +1,31 @@
 (define-module (momo services udevmon)
-  #:use-module (gnu packages)
-  #:use-module (gnu services)
   #:use-module (guix gexp)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages base)
+  #:use-module (gnu services)
   #:use-module (gnu services shepherd)
-  #:use-module (momo packages keyboard)
-  #:use-module (gnu packages linux)
+  #:use-module (gnu packages linux) ; Assuming interception-tools is in this module
   #:export (udevmon-service))
-
-(define (udevmon-shepherd-config)
-  '())
 
 (define udevmon-shepherd-service
   (lambda (config)
     (list (shepherd-service
-	   (documentation "udevmon daemon")
-	   (provision '(udevmon))
-	   (requirement '(udev))
-	   (start #~(make-forkexec-constructor
-		     (list (string-append #$interception-tools "/bin/udevmon" " -c " "/etc/udevmon.yaml"))
-		     (display #$interception-tools)
-		     #:environment-variables '("GUILE_WARN_DEPRECATED=detailed")
-		     #:log-file "/var/log/udevmon.log"))
-	   (stop #~(make-kill-destructor))))))
+           (documentation "udevmon from interception-tools.")
+           (provision '(udevmon))
+           (requirement '(udev)) ; or other relevant requirements
+           (start #~(make-forkexec-constructor
+                     (list #$(file-append interception-tools "/bin/udevmon")
+			   "-c" "/etc/udevmon.yaml")
+		     #:log-file "/var/log/udevmon.log"
+		     #:user "root"))
+           (stop #~(make-kill-destructor))))))
 
 (define udevmon-service-type
-  (service-type
-   (name 'udevmon)
-   (description "Start udevmon")
-   (extensions
-    (list (service-extension shepherd-root-service-type
-			     udevmon-shepherd-service)))))
+  (service-type (name 'udevmon)
+                (description "udevmon service from interception-tools.")
+                (extensions
+                 (list (service-extension shepherd-root-service-type
+                                          udevmon-shepherd-service)))))
 
 (define* (udevmon-service #:key (config (list)))
   (service udevmon-service-type config))
-
