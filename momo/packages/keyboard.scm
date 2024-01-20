163ddc3eacad2c3cf2c15232db95306f89161e88
Samuel Willey <samuel@thewilley.family>
2024-01-19 20:54:54 -0600

    Attempt at a spotify package

--- momo/packages/keyboard.scm
+++ momo/packages/keyboard.scm
@@ -8,28 +8,28 @@
 (define-public caps2esc
   (let ((commit "975bb742841244141012c05e0c624672160af3e0"))
     (package
-   (name "caps2esc")
-   (version "0.3.2")
-   (source (origin
-	    (method git-fetch)
-	    (uri (git-reference
-		  (url "https://gitlab.com/interception/linux/plugins/caps2esc")
-		  (commit commit)))
-	
-	    (sha256
-	     (base32
-	      "0y8cyx6g5ixfwh73mwg03ajy9d1i7fimqxn9q79k925i12a49wc0"))))
-   (build-system cmake-build-system)
-   (arguments
-    `(#:tests? #false))
-   (propagated-inputs
-    (list interception-tools))
-   (synopsis "Transforming the most useless key ever in the most useful one.
+     (name "caps2esc")
+     (version "0.3.2")
+     (source (origin
+	      (method git-fetch)
+	      (uri (git-reference
+		    (url "https://gitlab.com/interception/linux/plugins/caps2esc")
+		    (commit commit)))
+	      
+	      (sha256
+	       (base32
+		"0y8cyx6g5ixfwh73mwg03ajy9d1i7fimqxn9q79k925i12a49wc0"))))
+     (build-system cmake-build-system)
+     (arguments
+      `(#:tests? #false))
+     (propagated-inputs
+      (list interception-tools))
+     (synopsis "Transforming the most useless key ever in the most useful one.
 For vi/Vim/NeoVim addicts at least.")
-   (description
-    "Put what's useless in its place
+     (description
+      "Put what's useless in its place
 By moving the CAPSLOCK function to the far ESC location.
 Make what's useful comfortably present, just below your Pinky
 By moving both ESC and CTRL functions to the CAPSLOCK location")
-   (home-page "https://gitlab.com/interception/linux/plugins/caps2esc")
-   (license gpl3+))))
+     (home-page "https://gitlab.com/interception/linux/plugins/caps2esc")
+     (license gpl3+))))
---
+++ momo/packages/music.scm
@@ -0,0 +1,60 @@
+(define-module (momo packages music)
+  #:use-module (momo licenses)
+  #:use-module (guix licenses)
+  #:use-module (guix packages)
+  #:use-module (gnu packages linux)
+  #:use-module (guix build-system copy)
+  #:use-module (guix download))
+
+(define-public spotify
+  (let ((commit "g36b715a1"))
+    (package
+     (name "spotify")
+     (version "1.2.26.1187")
+     (source (origin
+	      (method url-fetch)
+	      (uri (string-append "http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_"
+				  version
+				  "."
+				  commit
+				  "_amd64.deb"))
+	      (sha256
+	       (base32
+		"0l8ixgwni27vyf538pqxfnfyylyvgb5min5n20qkaj2kss5xzaw3"))))
+     (build-system copy-build-system)
+     (arguments
+      `(#:tests #false
+	#:phases
+	(modify-phases %standard-phases
+		       (add-before 'unpack 'undeb
+				   (lambda* (#:key source #:allow-other-keys)
+				     (invoke "ar" "x" source)
+				     #t))
+		       (replace 'unpack 'unpack
+				(lambda* (#:allow-other-keys)
+				  (invoke "tar" "xvf" "./data.tar.gz")
+				  #t))
+		       (add-after 'unpack 'install
+				  (lambda* (#:key outputs #:allow-other-keys)
+				    (let* ((out (assoc-ref outputs "out"))
+					   (bin (string-append out "/bin"))
+					   (share (string-append out "/share/spotify"))
+					   (icons (string-append out "/share/icons"))
+					   (applications (string-append out "/share/applications")))
+				      (mkdir-p bin)
+				      (mkdir-p share)
+				      (mkdir-p icons)
+				      (mkdir-p applications)
+				      ;; Install the Spotify executable
+				      (copy-recursively "usr/share/spotify" share)
+				      (symlink (string-append share "/spotify") (string-append bin "/spotify"))
+				      ;; Install icons and desktop files
+				      (copy-recursively "usr/share/doc" (string-append out "/share/doc"))
+				      (copy-file "usr/share/spotify/spotify.desktop" (string-append applications "/spotify.desktop"))
+				      #t))))))
+     (synopsis "Spotify is a digital music streaming service that offers users access to millions of songs, podcasts, and videos from artists all over the world. It provides both free and premium subscription options, allowing users to listen to content with varying levels of control and without the need for downloading.")
+     (description
+      "Spotify is a popular streaming platform that allows users to access a vast library of music, podcasts, and videos. Users can create and share playlists, discover new music through personalized recommendations, and connect with artists' profiles. The platform operates on a freemium model, offering a free, ad-supported version and a premium version with additional features like offline listening and ad-free playback. Spotify integrates social features, enabling users to see what their friends are listening to and share music on social media. It's accessible on various devices, including smartphones, computers, and smart speakers, providing a seamless listening experience.
+")
+     (home-page "https://spotify.com")
+     (license (nonfree "https://www.spotify.com/us/legal/end-user-agreement/")))))
